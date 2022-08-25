b6a3509c9cd305bd6b64478e557c50a2c56cb29b
DohBae <bdoherty92@msn.com>
2022-08-25 16:18:34 +0000

    Fix bugs with sanitizing user input

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,22 @@ const pool = new Pool({
 });
 
 const process = require('process');
-const args = process.argv[2];
 
-pool.query(`
+const cohortName = process.argv[2];
+const values = [`${cohortName}`]
+
+const queryString =`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${args || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`
+
+pool
+.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
